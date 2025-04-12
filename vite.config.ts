7b6b588ed4c69f41a2eb7b27c74d8879895b82d6
AHBRIJESH <ahbrijesh2004@gmail.com>
2025-04-12 13:28:23 +0530

    Edit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/career-canvas-project/",
   server: {
     host: "::",
     port: 8080,
